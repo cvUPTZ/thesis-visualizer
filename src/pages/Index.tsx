ede9c5f1e99ac5a41a5fc982d9e54bd4d79964f3
cvUPTZ <cvupdz@gmail.com>
2024-12-30 09:15:10 +0100

    s

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,25 @@
+// File: /src/pages/Index.tsx
+import { Button } from "@/components/ui/button";
 import { ThesisEditor } from "@/components/ThesisEditor";
+import { useNavigate } from "react-router-dom";
 
 const Index = () => {
-  return <ThesisEditor />;
+  const navigate = useNavigate();
+  
+  const handleCreateThesisClick = () => {
+    navigate("/create-thesis");
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+        <div className="flex justify-end p-4">
+             <Button onClick={handleCreateThesisClick}>Create New Thesis</Button>
+        </div>
+        <div className="flex-1">
+          <ThesisEditor />
+        </div>
+    </div>
+  );
 };
 
 export default Index;
