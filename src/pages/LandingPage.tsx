4360f494687658454784ef8f157f792197fa5f01
cvUPTZ <cvupdz@gmail.com>
2025-01-05 10:48:45 +0100

    f

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,13 +3,13 @@ import { HeroSection } from "@/components/landing/HeroSection";
 import { FeaturesSection } from "@/components/landing/FeaturesSection";
 import { PricingSection } from "@/components/landing/PricingSection";
 import { Footer } from "@/components/landing/Footer";
-import { ThesisVisualization } from "@/components/landing/ThesisVisualization";
+import { EnhancedThesisVisualization } from "@/components/landing/ThesisVisualization";
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen bg-[#0A0D14]">
       <HeroSection />
-      <ThesisVisualization className="border-t border-gray-800" />
+      <EnhancedThesisVisualization className="border-t border-gray-800" />
       <FeaturesSection />
       <PricingSection />
       <Footer />
