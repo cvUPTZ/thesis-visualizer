d750c5b42a5466fda2174aca7559c051c8b9ee0f
cvUPTZ <cvupdz@gmail.com>
2025-01-05 10:32:24 +0100

    b

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,13 +3,13 @@ import { HeroSection } from "@/components/landing/HeroSection";
 import { FeaturesSection } from "@/components/landing/FeaturesSection";
 import { PricingSection } from "@/components/landing/PricingSection";
 import { Footer } from "@/components/landing/Footer";
-import { ThesisVisualizationDemo } from "@/components/landing/ThesisVisualization";
+import { ThesisVisualization } from "@/components/landing/ThesisVisualization";
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen bg-[#0A0D14]">
       <HeroSection />
-      <ThesisVisualizationDemo />
+      <ThesisVisualization />
       <FeaturesSection />
       <PricingSection />
       <Footer />
