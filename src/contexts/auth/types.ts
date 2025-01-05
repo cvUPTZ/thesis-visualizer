76e647a4e3e02fcbe7c1ac6b2d653aab41d9bfc1
cvUPTZ <cvupdz@gmail.com>
2025-01-05 15:54:14 +0100

    jhj

--- src/contexts/auth/types.ts
+++ src/contexts/auth/types.ts
@@ -1,14 +1,8 @@
-import { Dispatch, SetStateAction } from 'react';
-
 export interface AuthContextType {
   userId: string | null;
   userEmail: string | null;
-  userRole: string | null;
   loading: boolean;
-  isAuthenticated: boolean;
   logout: () => Promise<void>;
-  setUserId: Dispatch<SetStateAction<string | null>>;
-  setUserEmail: Dispatch<SetStateAction<string | null>>;
-  setUserRole: Dispatch<SetStateAction<string | null>>;
-  setLoading: Dispatch<SetStateAction<boolean>>;
+  isAuthenticated: boolean;
+  userRole: string | null;
 }
