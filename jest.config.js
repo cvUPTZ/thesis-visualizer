dcfca8b0c9d27709c346d99b30b4058f47f790c4
cvUPTZ <cvupdz@gmail.com>
2025-01-22 08:30:49 +0100

    Enhanced error handling and validations in SectionManagers and content generation

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\.(ts|tsx)$': 'ts-jest',
-    '^.+\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
 };
